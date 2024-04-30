22302833c7a7cbf95fc592bec29625e4db04ed3a
Christian Koch Echeverria <ko.che@web.de>
2024-04-30 04:02:09 +0200

    27: fixed sidebar interactivity

--- app/cv/sidebar/sidebar.tsx
+++ app/cv/sidebar/sidebar.tsx
@@ -29,7 +29,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ sidebarContent }) => {
                 {content.children ? (
                   <ul>
                     {content.children.map((child) => (
-                      <li key={`sidebar ${child.id}`} className="m-4">
+                      <li key={`sidebar ${child.id}`} className="mx-4 mt-4">
                         <SidebarElement
                           href={`#${child.id}`}
                           isActive={isSidebarContentActive(
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ scrollBehavior: "smooth" }}>
       <head>
         <link href="/favicon.ico" rel="icon" sizes="any" />
       </head>
